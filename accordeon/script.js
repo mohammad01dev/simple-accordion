2f7e489b230c2aaf528bd2972d81afca5e50f646
mohammad sadeghi <116999989+mohammadcoder82@users.noreply.github.com>
2023-02-10 11:50:34 +0330

    javascript

---
+++ accordeon/script.js
@@ -0,0 +1,10 @@
+// Writen By Github @mohammadcoder82;
+
+let accardion = document.querySelectorAll('.accordeon');
+
+accardion.forEach(acc => {
+	acc.addEventListener('click', ev => {
+		let panel = ev.target.nextElementSibling;
+		panel.classList.toggle('active');
+	})
+})
